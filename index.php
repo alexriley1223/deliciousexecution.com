35044d9b7d58bc8bb785e4c33e1d713123b030a6
Alex Riley <alexriley1223@gmail.com>
2022-12-17 23:28:01 -0500

    initial commit; placeholder splash page

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <meta name="description" content="Delicious Execution - just a couple of gamers">
+        <meta name="keywords" content="Delicious, Execution, Gaming, Discord, Arma, Rust">
+        <meta name="author" content="Delicious Execution">
+
+        <title>Delicious Execution</title>
+
+        <link rel="stylesheet" href="/resources/css/normalize.css" type="text/css"></link>
+        <link rel="stylesheet" href="/resources/css/app.css" type="text/css"></link>
+
+        <!-- Favicon -->
+        <link rel="apple-touch-icon" sizes="180x180" href="/resources/media/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/resources/media/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/resources/media/favicon-16x16.png">
+        <link rel="manifest" href="/resources/media/site.webmanifest">
+        <link rel="mask-icon" href="/resources/media/safari-pinned-tab.svg" color="#5bbad5">
+        <link rel="shortcut icon" href="/resources/media/favicon.ico">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="msapplication-config" content="/resources/media/browserconfig.xml">
+        <meta name="theme-color" content="#000000">
+    </head>
+    <body>
+        
+        <main>
+            <!-- Waves -->
+            <section id="waves"></section>
+
+            <section id="content">
+                <div class="flex-row">
+                    <h1>Delicious Execution</h1>
+
+                    <iframe src="https://discord.com/widget?id=604872185391087629&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+                </div>
+            </section>
+
+        </main>
+
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/88/three.min.js"></script>
+        <script src="/resources/js/app.js"></script>
+    </body>
+</html>
